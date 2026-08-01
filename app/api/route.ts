f8396f47351758ece6d266e94946acbdfbbce873
akarpovan <akarpovan@gmail.com>
2026-07-31 21:25:53 -0500

    add list products anna

--- app/api/route.ts
+++ app/api/route.ts
@@ -11,17 +11,17 @@ const pool = new Pool({
 export async function GET() {
   try {
     const client = await pool.connect();
-    
-   
+
+
     const result = await client.query(`
       SELECT p.*, u.name as seller_name, sp.profile_image 
       FROM products p
       JOIN users u ON p.seller_id = u.id
       LEFT JOIN seller_profiles sp ON u.id = sp.seller_id
     `);
-    
+
     client.release();
-    
+
     // Return the data as JSON
     return NextResponse.json(result.rows);
   } catch (error) {
